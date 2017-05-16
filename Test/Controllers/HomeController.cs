95bd346f90e8d18dd549cff9b044425785a2d331
rogertinsley <mail@rogertinsley.co.uk>
2017-05-16 21:48:20 +0100

    :sunrise:

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Test.Model;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+			using (var db = new BloggingContext())
+			{
+				var blogs = db.Blogs
+					.OrderBy(b => b.Url)
+					.ToList();
+
+				return View(blogs);
+			}
+        }
+
+        public IActionResult About()
+        {
+			using (var db = new BloggingContext())
+			{
+				var blog = new Blog { Url = "http://sample.com" };
+				db.Blogs.Add(blog);
+				db.SaveChanges();
+			}
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Test/Migrations/20170516204244_InitialCreate.Designer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Test.Model;
+
+namespace Test.Migrations
+{
+    [DbContext(typeof(BloggingContext))]
+    [Migration("20170516204244_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("Test.Model.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Rating");
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blogs");
+                });
+
+            modelBuilder.Entity("Test.Model.Post", b =>
+                {
+                    b.Property<int>("PostId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BlogId");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("PostId");
+
+                    b.HasIndex("BlogId");
+
+                    b.ToTable("Posts");
+                });
+
+            modelBuilder.Entity("Test.Model.Post", b =>
+                {
+                    b.HasOne("Test.Model.Blog", "Blog")
+                        .WithMany("Posts")
+                        .HasForeignKey("BlogId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
